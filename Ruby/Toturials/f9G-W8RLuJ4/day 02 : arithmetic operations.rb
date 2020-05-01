247f0b604e8adae04e50fd8cc0333ca508a3cee2
Ahmed <40142170+ahmedGHdeeb@users.noreply.github.com>
2020-05-01 12:38:03 +0300

    Create day 02 : arithmetic operations.rb

---
+++ Ruby/Toturials/f9G-W8RLuJ4/day 02 : arithmetic operations.rb
@@ -0,0 +1,20 @@
+"day 02 : arithmetic operations"
+
+
+"addition"
+puts 1 + 1
+
+"subtraction"
+puts 3 - 1
+
+"division"
+puts 1.0 / 2
+
+"mutiplication"
+puts 2 * 2.3
+
+"power"
+puts 4 ** 3
+
+"modules"
+puts 3 % 2
