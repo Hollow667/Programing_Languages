e8ecb3af50792a373c31176016a9794dbaa36fea
Ahmed <40142170+ahmedGHdeeb@users.noreply.github.com>
2020-05-01 20:52:48 +0300

    Create working on strings.rb

---
+++ Ruby/Toturials/f9G-W8RLuJ4/working on strings.rb
@@ -0,0 +1,8 @@
+str = "string"
+puts str.downcase   #string#     
+puts str.upcase     #STRING#
+puts str.length     #6#
+puts str.capitalize #String#
+puts str.reverse    #gnirts#
+puts str[0]         #s#
+puts str[1..4]      #trin#
