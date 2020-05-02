ac35821ec102ea890424590ea7b37f0d411bf1cd
Ahmed <40142170+ahmedGHdeeb@users.noreply.github.com>
2020-05-02 07:23:12 +0300

    Update and rename day 01 : puts print variable.rb to day 01 08 : puts print gets variable.rb

---
+++ Ruby/Toturials/f9G-W8RLuJ4/day 01 08 : puts print gets variable.rb
@@ -0,0 +1,33 @@
+puts "Hello world!"
+print "no new line"
+
+#declare a variable
+var = 3
+puts var
+
+var = "string"
+puts var
+
+puts "enter first string..."
+
+instr1 = gets
+
+puts "enter second string..."
+
+instr2 = gets
+
+puts instr1  + instr2
+
+#will print the two inputs with a new line between them#
+
+puts "enter first word..."
+
+instr1 = gets.chomp
+
+puts "enter second word..."
+
+instr2 = gets.chomp
+
+puts instr1 + instr2
+
+#will pront the two inputs with out '\n'#
--- Ruby/Toturials/f9G-W8RLuJ4/day 01 : puts print variable.rb
+++
@@ -1,9 +0,0 @@
-puts "Hello world!"
-print "no new line"
-
-#declare a variable
-var = 3
-puts var
-
-var = "string"
-puts var
