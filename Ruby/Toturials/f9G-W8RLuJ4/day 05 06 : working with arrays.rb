d5e3e10b9afdfbc2d8483cac4726154c38a1af24
Ahmed <40142170+ahmedGHdeeb@users.noreply.github.com>
2020-05-03 10:36:51 +0300

    Update and rename day 05 06 : working with arrays.rb to day 05 06 11 : working with arrays.rb

--- Ruby/Toturials/f9G-W8RLuJ4/day 05 06 : working with arrays.rb
+++
@@ -1,9 +0,0 @@
-arr = ["data1", "data2", "data3"]
-arr[0] = "datam1"
-puts arr     #all array#
-puts arr[0]  #first element#
-puts arr[-1] #last element#
-
-#multi type items#
-arr2 = [1,1.2,"str"]
-puts arr2
