1a2f9f73cc633c6d5453cd8f68e9346279637c44
Ahmed <40142170+ahmedGHdeeb@users.noreply.github.com>
2020-05-05 07:51:16 +0300

    Update day 07 : what is hash.rb

--- Ruby/Toturials/f9G-W8RLuJ4/day 07 : what is hash.rb
+++ Ruby/Toturials/f9G-W8RLuJ4/day 07 : what is hash.rb
@@ -11,3 +11,10 @@ hashName = Hash.new
 hashName["key1"]="val new1"
 hashName["key2"]="val new2"
 puts hashName
+
+#sorting by value
+hashName2 = {
+    'key1' => 3,
+    'key2' => 2
+    }
+hashName2.sort_by(|key, val|val)
