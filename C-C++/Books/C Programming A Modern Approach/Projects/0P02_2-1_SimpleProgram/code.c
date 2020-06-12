f5143b3df2f726a7b0b1c884b85edc29a3f43e4b
Ahmed <40142170+ahmedGHdeeb@users.noreply.github.com>
2020-06-12 03:03:29 -0700

    Add files via upload

---
+++ C-C++/Books/C Programming A Modern Approach/Projects/0P02_2-1_SimpleProgram/code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//directives
+int main(int args, char* argv[])
+{
+  // statements
+  printf("To C, or not to C: that is the question");
+  return 0;
+}
---
+++ C-C++/Books/C Programming A Modern Approach/Projects/0P02_2-3_Comments/code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*Author: K. N. King*/
+int main(int args, char* argv[])
+{
+  // statements
+  printf("To C, or not to C: that is the question");
+  return 0;
+}
